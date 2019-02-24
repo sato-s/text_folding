3776dad44a4cbee542c5725c79f0a27443bb58bb
sato.sawahiko <sato.sawahiko@jp.fujitsu.com>
2019-02-24 16:36:43 +0000

    Text Wrap

---
+++ lib/old.ex
@@ -0,0 +1,144 @@
+# defmodule TextFolding do
+#   use Memoize
+#   @limit 6
+#   def fold(remaining_words, lengths_array) when length(remaining_words) == 0 do
+#     lengths_array
+#     |> TextFolding.cost
+#     |> IO.inspect(label: "Sum cost")
+#   end
+#
+#   # TODO
+#   # this just returns possible folding combination
+#   def fold(remaining_words, lengths_array) do
+#     [head | tail] = remaining_words
+#     new_line = append_new_line(lengths_array, head)
+#     same_line = append_same_line(lengths_array, head)
+#     fold(tail, same_line)
+#     fold(tail, new_line)
+#   end
+#
+#   # Append a word as a new line
+#   # [[1], [1]] => [[1], [1], [1]]
+#   def append_new_line(lengths_array, word) do
+#     lengths_array ++ [[word]]
+#   end
+#
+#   # Append a word as a last word of existing line
+#   # [[1], [1]] => [[1], [1, 1]]
+#   def append_same_line(lengths_array, word) do
+#     init = Enum.take(lengths_array, length(lengths_array) -1)
+#     last = Enum.at(lengths_array, -1) ++ [word]
+#     init ++ [last]
+#   end
+#
+#   # Calculate cost of folding of entire text
+#   # Input: work length array of text
+#   # Ex: [[10, 10, 9], [10, 18]]
+#   # Output: cost of the text
+#   def cost(lengths_array) do
+#     trailing_spaces(lengths_array)
+#     |> Enum.reduce(0, fn (x, acc) -> :math.pow(x, 3) + acc end)
+#   end
+#
+#   # Input length array, returns white spaces array
+#   def trailing_spaces([[]]) do
+#     []
+#   end
+#
+#   defmemo trailing_spaces(lengths_array) do
+#     {previous_lengths_array, current, popped_new_line?} = TextFolding.pop(lengths_array)
+#     if current >= @limit do
+#       [100_000_000]
+#     end
+#     previous_white_spaces = trailing_spaces(previous_lengths_array)
+#
+#     if popped_new_line? do
+#       previous_white_spaces ++ [@limit - current]
+#     else
+#       while_spaces_of_last_line = List.last(previous_white_spaces)
+#                                   |> (&(&1 - (current + 1))).()
+#       if while_spaces_of_last_line < 0 do
+#         [100_000_000]
+#       else
+#         previous_white_spaces
+#         |> Enum.drop(-1)
+#         |> (fn(x) ->  x ++ [while_spaces_of_last_line] end).()
+#       end
+#     end
+#   end
+#
+#   def pop([[]]) do
+#     raise "Can't pop any more"
+#   end
+#
+#   def pop([[x]]) do
+#     {[[]], x, true}
+#   end
+#
+#   def pop(lengths_array) do
+#     init = Enum.drop(lengths_array, -1)
+#     last = List.last(lengths_array)
+#     if length(last) == 1 do
+#       {init, List.first(last), true}
+#     else
+#       {init ++ [Enum.take(last, length(last) - 1)], List.last(last), false}
+#     end
+#   end
+#
+#   # Count unnecessary trailing white spaces
+#   defmemo line_cost(lengths) do
+#     c = @limit - (Enum.sum(lengths) + Enum.count(lengths) - 1)
+#     if c < 0 do
+#       # Return (almost) infinity cost if this exceeds @limit
+#       100_000_000
+#     else
+#       :math.pow(c, 3)
+#     end
+#   end
+#
+#   def to_length_array(string) do
+#     String.split(string, " ")
+#       |> Enum.map(&String.length/1)
+#   end
+#
+# end
+#
+# # string = "aaa bb cc ddddd"
+#
+# # TextFolding.to_length_array(string)
+# #   |> IO.inspect
+# #
+# # TextFolding.line_cost([1,2])
+# #   |> IO.inspect
+# #
+# # TextFolding.cost([[3, 2], [2], [5] ])
+# #   |> IO.inspect
+# #
+# # TextFolding.cost([[3], [2, 2], [5] ])
+# #   |> IO.inspect
+# #
+# # TextFolding.append_new_line([[1], [1]], 1)
+# #   |> IO.inspect
+# #
+# # TextFolding.append_same_line([[1], [1]], 1)
+# #   |> IO.inspect
+# # lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+# #
+# # lorem = "aa bb cccc asa"
+#
+#   #
+# Application.ensure_all_started(:memoize)
+# IO.inspect "---"
+# TextFolding.trailing_spaces([[1,2],[3, 2, 1]])
+#   |> IO.inspect
+#
+# IO.inspect "---"
+# TextFolding.cost([[1,2],[3, 2, 1]])
+#   |> IO.inspect
+#
+# IO.inspect "lorem"
+# # lorem = "aaa bb cc ddddd"
+# lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+# String.split(lorem, " ")
+#   |> Enum.map(&String.length/1)
+#   |> TextFolding.fold([[]])
--- lib/text_folding.ex
+++ lib/text_folding.ex
@@ -1,93 +1,89 @@
-defmodule TextFolding do
-  @limit 6
-  # strings => [2, 4, 5, 6]
-  # F[]
-  def fold(remaining_words, lengths_array) when length(remaining_words) == 0 do
-    # Enum.each(lengths_array, &(IO.inspect &1, charlists: :as_lists, label: "line"))
-    # Enum.each(lengths_array, &(IO.inspect line_cost(&1), label: "cost"))
-    lengths_array
-    |> TextFolding.cost
-    |> IO.inspect(label: "Sum cost")
+defmodule TextWrap do
+  use Memoize
+  @limit 30
+  @infinity 100_000_000_000
+
+  def wrap(string) do
+    words = String.split(string, " ")
+    lengths = Enum.map(words, &String.length/1)
+    {cost, wraps} = TextWrap.cost(lengths, length(lengths) - 1)
+    IO.inspect {cost, wraps}, label: "cost"
+
+    # Print wrapped text
+    Enum.chunk_every([1] ++ wraps ++ [length(words)], 2, 1,:discard)
+      |> Enum.map(fn(x) ->
+        from = List.first(x)
+        to = List.last(x)
+        if from == 1 do
+          Enum.slice(words, (0..to))
+        else
+          Enum.slice(words, (from+1..to))
+        end
+      end)
+      |> Enum.each(fn(words) ->
+        line =  Enum.join(words, " ")
+        len = String.length(line)
+        # white_spaces = String.duplicate(" ", @limit - len)
+        # IO.puts "#{line}#{white_spaces}(#{len}/#{@limit})"
+        white_spaces = String.duplicate(".", 30 - len)
+        IO.puts "#{line}#{white_spaces}(#{len}/#{@limit})"
+      end)
   end
 
-  # TODO
-  # this just returns possible folding combination
-  def fold(remaining_words, lengths_array) do
-    [head | tail] = remaining_words
-    new_line = append_new_line(lengths_array, head)
-    same_line = append_same_line(lengths_array, head)
-    fold(tail, same_line)
-    fold(tail, new_line)
+  # Calculate optimized accumulated cost from 0 to n
+  def cost(lengths, n) when n == 0 do
+    # Just return 0 if there's no word
+    {TextWrap.line_cost(lengths, 0, 0), []}
   end
 
-  # Append a word as a new line
-  # [[1], [1]] => [[1], [1], [1]]
-  def append_new_line(lengths_array, word) do
-    lengths_array ++ [[word]]
+  defmemo cost(lengths, n) do
+    costs = (0..n-1)
+      |> Enum.map(
+        fn(wrap) ->
+          {prev_cost, prev_wrap} = cost(lengths, wrap)
+          new_line_cost = TextWrap.line_cost(lengths, wrap + 1, n)
+          cost = prev_cost + new_line_cost
+          wraps = prev_wrap ++ [wrap]
+          {cost, wraps}
+        end)
+    one_line_cost = {TextWrap.line_cost(lengths, 0, n), []}
+    [one_line_cost | costs]
+      |> Enum.min_by(fn({cost, _wraps}) -> cost end)
   end
 
-  # Append a word as a last word of existing line
-  # [[1], [1]] => [[1], [1, 1]]
-  def append_same_line(lengths_array, word) do
-    init = Enum.take(lengths_array, length(lengths_array) -1)
-    last = Enum.at(lengths_array, -1) ++ [word]
-    init ++ [last]
-  end
-
-  # Calculate cost of folding of entire text
-  # Input: work length array of text
-  # Ex: [[10, 10, 9], [10, 18]]
-  # Output: cost of the text
-  def cost(lengths_array) do
-    Enum.map(lengths_array, &TextFolding.line_cost/1)
-    |> Enum.sum
-  end
-
-  # Count unnecessary trailing white spaces
-  def line_cost(lengths) do
-    c = @limit - (Enum.sum(lengths) + Enum.count(lengths) - 1)
-    if c < 0 do
-      # Return (almost) infinity cost if this exceeds @limit
-      100_000_000
+  def line_cost(lengths, from, to) do
+    if trailing_white_spaces(lengths, from, to) < 0 do
+      # Return (almost) infinity cost if words in from-to can't arrange in one line.
+      @infinity
     else
-      :math.pow(c, 3)
+      :math.pow(trailing_white_spaces(lengths, from, to), 3)
     end
   end
 
-  def to_length_array(string) do
-    String.split(string, " ")
-      |> Enum.map(&String.length/1)
+  # Count trailing white spaces of from-to in lengths array
+  def trailing_white_spaces(lengths, from, to) when from == to do
+    # if from == to, we only have one word.
+    # Just return limit minus word length
+    @limit - Enum.at(lengths, from)
+  end
+
+  def trailing_white_spaces(_, from, to) when from > to do
+    raise "Something is very wrong"
+  end
+
+  def trailing_white_spaces(_, from, to) when from > to do
+    raise "Something is very wrong"
+  end
+
+  defmemo trailing_white_spaces(lengths, from, to) when from != to do
+    trailing_white_spaces(lengths, from, to - 1) - Enum.at(lengths, to) - 1
   end
 
 end
 
-# string = "aaa bb cc ddddd"
+Application.ensure_all_started(:memoize)
 
-# TextFolding.to_length_array(string)
-#   |> IO.inspect
-#
-# TextFolding.line_cost([1,2])
-#   |> IO.inspect
-#
-# TextFolding.cost([[3, 2], [2], [5] ])
-#   |> IO.inspect
-#
-# TextFolding.cost([[3], [2, 2], [5] ])
-#   |> IO.inspect
-#
-# TextFolding.append_new_line([[1], [1]], 1)
-#   |> IO.inspect
-#
-# TextFolding.append_same_line([[1], [1]], 1)
-#   |> IO.inspect
-lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+# target = "aaa bb cc dddddd"
+target = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-# String.split(string, " ")
-#   |> Enum.map(&String.length/1)
-#   |> TextFolding.fold([[]])
-#   |> IO.inspect
-  #
-String.split(lorem, " ")
-  |> Enum.map(&String.length/1)
-  |> TextFolding.fold([[]])
-  |> IO.inspect
+TextWrap.wrap(target)
