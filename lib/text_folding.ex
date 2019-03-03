13fc9b8973f82992c0254293dd20a2b0a1e372d3
sato.sawahiko <sato.sawahiko@jp.fujitsu.com>
2019-03-03 11:20:36 +0000

    Refactored

--- lib/text_folding.ex
+++ lib/text_folding.ex
@@ -3,38 +3,46 @@ defmodule TextFolding do
   @limit 20
   @infinity 100_000_000_000
 
-  # Calculate accumulated cost from 0 to j
-  def min_cost(_lengths, j) when j == 0 do
+  # Calculate optimized accumulated cost from 0 to n
+  def min_cost(_lengths, n) when n == 0 do
+    # Just return 0 if there's no word
     0
   end
 
-  defmemo min_cost(lengths, j) do
-    (0..j-1)
-      |> Enum.map(fn(x) -> min_cost(lengths, x) + TextFolding.line_cost(lengths, x + 1, j) end)
+  defmemo min_cost(lengths, n) do
+    (0..n-1)
+      # We want to calulate all combination of folding.
+      # We fold text at posision x in the bollow function.
+      # min_cost(lengths, x) is minimum cost until x.
+      # line_cost(lengths, x + 1, n) is a cost of one line after x
+      |> Enum.map(fn(x) -> min_cost(lengths, x) + TextFolding.line_cost(lengths, x + 1, n) end)
+      # Take minimum cost
       |> Enum.min
   end
 
-  defmemo line_cost(lengths, i, j) do
-    if trailing_white_spaces(lengths, i, j) < 0 do
-      # Return (almost) infinity cost if words in i-j can't arrange in one line.
+  defmemo line_cost(lengths, from, to) do
+    if trailing_white_spaces(lengths, from, to) < 0 do
+      # Return (almost) infinity cost if words in from-to can't arrange in one line.
       @infinity
     else
-      :math.pow(trailing_white_spaces(lengths, i, j), 2)
+      :math.pow(trailing_white_spaces(lengths, from, to), 2)
     end
   end
 
-  def trailing_white_spaces(lengths, i, j) when i == j do
-    @limit - Enum.at(lengths, i)
+  # Count trailing white spaces of from-to in lengths array
+  def trailing_white_spaces(lengths, from, to) when from == to do
+    # if from == to, we only have one word.
+    # Just return limit minus word length
+    @limit - Enum.at(lengths, from)
   end
 
-  def trailing_white_spaces(lengths, i, j) when i != j do
-    trailing_white_spaces(lengths, i, j - 1) - Enum.at(lengths, j) - 1
+  def trailing_white_spaces(lengths, from, to) when from != to do
+    trailing_white_spaces(lengths, from, to - 1) - Enum.at(lengths, to) - 1
   end
 end
 
 Application.ensure_all_started(:memoize)
 
-# target = "aa bb cccc asa"
 target = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 lengths = String.split(target, " ")
