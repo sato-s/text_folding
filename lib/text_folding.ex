eef9894908305a77b9c02874612141c8d22f4e2f
sato sawahiko <sato.sawahiko@jp.fujitsu.com>
2019-03-04 06:06:12 +0900

    Fold -> wrap

--- lib/text_folding.ex
+++ lib/text_folding.ex
@@ -1,16 +1,16 @@
-defmodule TextFolding do
+defmodule TextWrap do
   use Memoize
   @limit 30
   @infinity 100_000_000_000
 
-  def fold(string) do
+  def wrap(string) do
     words = String.split(string, " ")
     lengths = Enum.map(words, &String.length/1)
-    {cost, foldings} = TextFolding.min_cost(lengths, length(lengths) - 1)
-    IO.inspect {cost, foldings}, label: "cost"
+    {cost, wrap_ats} = TextWrap.min_cost(lengths, length(lengths) - 1)
+    IO.inspect {cost, wrap_ats}, label: "cost"
 
-    # Print folded text
-    Enum.chunk_every([1] ++ foldings ++ [length(words)], 2, 1,:discard)
+    # Print wrapped text
+    Enum.chunk_every([1] ++ wrap_ats ++ [length(words)], 2, 1,:discard)
       |> Enum.map(fn(x) ->
         from = List.first(x)
         to = List.last(x)
@@ -31,29 +31,29 @@ defmodule TextFolding do
   # Calculate optimized accumulated cost from 0 to n
   def min_cost(lengths, n) when n == 0 do
     # Just return 0 if there's no word
-    {TextFolding.line_cost(lengths, 0, 0), []}
+    {TextWrap.line_cost(lengths, 0, 0), []}
   end
 
   defmemo min_cost(lengths, n) do
     (0..n)
-      # We want to calulate all combination of folding.
-      # We fold text at posision x in the bollow function.
-      # min_cost(lengths, fold_at) is minimum cost until fold_at.
-      # line_cost(lengths, fold_at + 1, n) is a cost of one line after fold_at
+      # We want to calulate all combination of text wraps.
+      # We wrap text at posision x in the bollow function.
+      # min_cost(lengths, wrap_at) is minimum cost until wrap_at.
+      # line_cost(lengths, wrap_at + 1, n) is a cost of one line after wrap_at
       |> Enum.map(
-        fn(fold_at) ->
-          if fold_at == n do
-            {TextFolding.line_cost(lengths, 0, n), []}
+        fn(wrap_at) ->
+          if wrap_at == n do
+            {TextWrap.line_cost(lengths, 0, n), []}
           else
-            {prev_cost, prev_fold_at} = min_cost(lengths, fold_at)
-            new_line_cost = TextFolding.line_cost(lengths, fold_at + 1, n)
+            {prev_cost, prev_wrap_at} = min_cost(lengths, wrap_at)
+            new_line_cost = TextWrap.line_cost(lengths, wrap_at + 1, n)
             cost = prev_cost + new_line_cost
-            foldings = prev_fold_at ++ [fold_at]
-            {cost, foldings}
+            wrap_ats = prev_wrap_at ++ [wrap_at]
+            {cost, wrap_ats}
           end
         end)
       # Take minimum cost
-      |> Enum.min_by(fn({cost, _foldings}) -> cost end)
+      |> Enum.min_by(fn({cost, _wrap_ats}) -> cost end)
   end
 
   def line_cost(lengths, from, to) do
@@ -81,4 +81,4 @@ Application.ensure_all_started(:memoize)
 
 target = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-TextFolding.fold(target)
+TextWrap.wrap(target)
